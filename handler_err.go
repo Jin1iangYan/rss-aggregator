8659511cb430b55214b4a6abf77a09f022ae8757
Jin1iangYan <yanmiku0206@outlook.com>
2024-08-04 16:33:20 +0800

    fix: respondWithErreo -> respondWithError

--- handler_err.go
+++ handler_err.go
@@ -3,5 +3,5 @@ package main
 import "net/http"
 
 func handlerErr(w http.ResponseWriter, r *http.Request) {
-	respondWithErreo(w, 400, "Something went worng")
+	respondWithError(w, 400, "Something went worng")
 }
